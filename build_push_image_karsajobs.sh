c51362bea773f3436185f66a1efe06eb831b2262
pinoezz <alfino.devops@ocatelkom.co.id>
2023-01-03 21:13:29 +0700

    add bash script

---
+++ build_push_image_karsajobs.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "Membuat Docker image"
+docker build -t pinoezz/karsajobs:latest  .
+
+echo "Melihat daftar image di lokal"
+docker images
+
+echo "Login ke docker Hub"
+echo $DOCKER_PASSWORD | docker login -u pinoezz --password-stdin
+
+echo "Mengunggah image ke Docker Hub"
+docker push pinoezz/karsajobs:latest
