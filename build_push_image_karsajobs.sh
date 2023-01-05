f57b7b96b0332c6d5020d14daa54ca3408122740
Alfino Dwi Nugroho <106061407+pinoezz@users.noreply.github.com>
2023-01-05 08:29:57 +0700

    Update build_push_image_karsajobs.sh

--- build_push_image_karsajobs.sh
+++ build_push_image_karsajobs.sh
@@ -7,7 +7,7 @@ echo "Melihat daftar image di lokal"
 docker images
 
 echo "Login ke docker Hub"
-echo $DOCKER_PASSWORD | docker login -T -u pinoezz --password-stdin
+echo $DOCKER_PASSWORD | docker login  -u pinoezz --password-stdin
 
 echo "Mengunggah image ke Docker Hub"
 docker push pinoezz/karsajobs:latest
